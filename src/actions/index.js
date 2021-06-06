005b3e98bfc29d68357060215b4e3cdce9184ce5
Viegas <mvrdgs@gmail.com>
2021-06-06 20:39:08 -0300

    exercicio 1

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,4 +7,13 @@ export const getPicture = (data) => ({ type: GET_PICTURE, data });
 
 export function fetchAPI() {
   // Desenvolva aqui o código da action assíncrona
+  return async (dispatch) => {
+    try {
+      dispatch(requestAPI());
+      const data = await (await fetch('https://aws.random.cat/meow')).json();
+      dispatch(getPicture(data));
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
